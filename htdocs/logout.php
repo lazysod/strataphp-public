34b3edfa19522d96ae89fbbb9398d5645423b0b5
Barry Smith <divinorum2001@gmail.com>
2025-08-21 15:50:09 +0100

    Added updated logout and updated config example

--- htdocs/logout.php
+++ htdocs/logout.php
@@ -1,5 +1,7 @@
 <?php
 // Global logout: destroy session and redirect to main site
+require_once __DIR__ . '/app/start.php'; // config, autoload, etc.
+$config = require __DIR__ . '/app/config.php';
 session_start();
 $_SESSION = [];
 if (ini_get('session.use_cookies')) {
@@ -10,6 +12,9 @@ if (ini_get('session.use_cookies')) {
         $params['secure'], $params['httponly']
     );
 }
+// Remove the remember me cookie
+setcookie(PREFIX . 'cookie_login', '', time() - 42000, '/', '', isset($_SERVER['HTTPS']), true);
+
 session_destroy();
 header('Location: /');
 exit;
