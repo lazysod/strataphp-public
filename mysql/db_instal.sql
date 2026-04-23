b8660fabcb7b2a0ce0e8da2a1e73eed1018d9088
Barry Smith <divinorum2001@gmail.com>
2026-04-23 13:28:58 +0100

    DB install fix

--- mysql/db_instal.sql
+++ mysql/db_instal.sql
@@ -1,15 +1,7 @@
 --
 -- Generation Time: Aug 21, 2025 at 09:43 AM
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 -- Table structure for table `login_tracker`
 DROP TABLE IF EXISTS `login_tracker`;
@@ -17,39 +9,31 @@ CREATE TABLE `login_tracker` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `user_id` int(255) NOT NULL,
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-  START TRANSACTION;
-  SET time_zone = "+00:00";
-  /*!40101 SET NAMES utf8mb4 */;
-
-  -- Table structure for `login_tracker`
-  DROP TABLE IF EXISTS `login_tracker`;
   `locked_at` timestamp NULL DEFAULT NULL,
   `locked_by` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Table structure for table `rank`
-  -- Table structure for `migrations`
-  DROP TABLE IF EXISTS `migrations`;
-  `user_id` int(255) NOT NULL,
-  `title` varchar(23) NOT NULL,
-  `level` int(3) DEFAULT '0',
-  `admin` int(1) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
   -- Table structure for `migration_lock`
-  DROP TABLE IF EXISTS `migration_lock`;
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `user_id` int(255) NOT NULL,
-  `key` varchar(255) NOT NULL,
-  `created_date` timestamp NULL DEFAULT NULL,
-  `expiry_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-  -- Table structure for `rank`
-  DROP TABLE IF EXISTS `rank`;
+DROP TABLE IF EXISTS `rank`;
+CREATE TABLE `rank` (
+    `id` int(255) NOT NULL AUTO_INCREMENT,
+    `user_id` int(255) NOT NULL,
+    `title` varchar(23) NOT NULL,
+    `level` int(3) DEFAULT '0',
+    `admin` int(1) DEFAULT '0',
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `migration_lock`;
+CREATE TABLE `migration_lock` (
+    `id` int(255) NOT NULL AUTO_INCREMENT,
+    `user_id` int(255) NOT NULL,
+    `key` varchar(255) NOT NULL,
+    `created_date` timestamp NULL DEFAULT NULL,
+    `expiry_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `display_name` varchar(100) DEFAULT NULL,
@@ -58,31 +42,28 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `pwd` varchar(128) NOT NULL,
   `security_hash` varchar(255) NOT NULL,
-  -- Table structure for `reset`
-  DROP TABLE IF EXISTS `reset`;
+  `avatar` varchar(120) DEFAULT 'public_uploads/blank.png',
+  `is_admin` int(1) DEFAULT '0',
+  `sys_admin` int(1) DEFAULT NULL,
   `rank` int(1) DEFAULT '0',
   `last_access` datetime DEFAULT NULL,
   `active` int(1) DEFAULT '0',
   `date` date DEFAULT NULL,
-  `dead_switch` int(1) DEFAULT '0',
+  `dead_switch` int(1) DEFAULT '0', 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-  -- Table structure for `users`
-  DROP TABLE IF EXISTS `users`;
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `user_id` int(255) NOT NULL,
-  `activation_key` varchar(255) NOT NULL,
-  `entry_date` datetime NOT NULL,
-  `expiry_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Table structure for table `user_sessions`
+DROP TABLE IF EXISTS `reset`;
+CREATE TABLE `reset` (
+    `id` int(255) NOT NULL AUTO_INCREMENT,
+    `user_id` int(255) NOT NULL,
+    `activation_key` varchar(255) NOT NULL,
+    `entry_date` datetime NOT NULL,
+    `expiry_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 DROP TABLE IF EXISTS `user_sessions`;
-
 CREATE TABLE `user_sessions` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `device_id` varchar(128) NOT NULL,
   `device_type` varchar(32) DEFAULT NULL,
@@ -92,41 +73,29 @@ CREATE TABLE `user_sessions` (
   `revoked` tinyint(1) DEFAULT '0',
   `last_seen` datetime NOT NULL,
   `created_at` datetime NOT NULL,
-  `expires_at` datetime DEFAULT NULL
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `device_id` (`device_id`),
+  KEY `session_token` (`session_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Indexes for table `user_sessions`
-ALTER TABLE `user_sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `device_id` (`device_id`),
-  ADD KEY `session_token` (`session_token`);
-  -- Table structure for `user_sessions`
-  DROP TABLE IF EXISTS `user_sessions`;
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 -- Table structure for table `links`
 DROP TABLE IF EXISTS `links`;
 CREATE TABLE `links` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `icon` varchar(64) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `order` int(11) NOT NULL DEFAULT '0',
-  `nsfw` tinyint(1) DEFAULT '0'
--- Dumping data for table `links`
+  `nsfw` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 INSERT INTO `links` (`id`, `title`, `url`, `icon`, `created_at`, `order`, `nsfw`) VALUES
 (1, 'Strata PHP Home Page', 'https://www.strataphp.org', 'fas fa-link', '2025-08-13 08:09:46', 2, 1),
 (3, 'B.Smith Home Page!', 'https://barrysmith.dev', 'fas fa-link', '2025-08-13 08:25:14', 1, 0),
-(4, 'Lazy Links 2.0', 'https://lazylinks.co.uk', 'fas fa-link', '2025-08-13 08:26:48', 4, 0),
--- Indexes for table `links`
-ALTER TABLE `links`
-  -- Table structure for `links`
-  DROP TABLE IF EXISTS `links`;
-ALTER TABLE `links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
+(4, 'Lazy Links 2.0', 'https://lazylinks.co.uk', 'fas fa-link', '2025-08-13 08:26:48', 4, 0);
 
 -- Table structure for `google_analytics_settings`
 DROP TABLE IF EXISTS `google_analytics_settings`;
@@ -138,28 +107,14 @@ CREATE TABLE `google_analytics_settings` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-START TRANSACTION;
-SET time_zone = "+00:00";
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
-  -- Table structure for `cookie_login`
-  CREATE TABLE `cookie_login` (
-    `id` int(255) NOT NULL,
-    `user_id` int(255) NOT NULL,
-    `cookie_hash` varchar(255) NOT NULL,
-    `date_added` date NOT NULL,
-    `last_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `expiry_date` date NOT NULL
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  ALTER TABLE `cookie_login`
-    ADD PRIMARY KEY (`id`);
-  ALTER TABLE `cookie_login`
-    MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-  COMMIT;
+DROP TABLE IF EXISTS `cookie_login`;
+CREATE TABLE `cookie_login` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `user_id` int(255) NOT NULL,
+  `cookie_hash` varchar(255) NOT NULL,
+  `date_added` date NOT NULL,
+  `last_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `expiry_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --
