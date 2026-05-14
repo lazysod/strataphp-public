36e762948eb50c35a9c27b35dd04f9c2d8525417
Barry Smith <divinorum2001@gmail.com>
2026-05-14 20:42:19 +0100

    v1.0.5: Zero-config install using .env + auto-copy config-example.php

--- public_html/app/config-example.php
+++ public_html/app/config-example.php
@@ -1,4 +1,8 @@
 <?php
+if (file_exists(dirname(__DIR__, 2) . '/vendor/autoload.php')) {
+    require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+    Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2))->safeLoad();
+}
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', dirname(__DIR__, 2));
 }
